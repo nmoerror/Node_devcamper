814897ecffc790ec64ae68f28084c5cdfdcda276
Alejandro Gonzalez <Alejandro@nomarginoferror.com>
2019-11-10 07:50:10 +1300

    routes

--- server.js
+++ server.js
@@ -9,10 +9,41 @@ const todos = [
 ];
 
 const server = http.createServer((req, res) => {
-  const { headers, url, method } = req;
+  const { url, method } = req;
+  let body = [];
 
-  res.writeHead(404, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ success: false, error: 'Not Found', data: null }));
+  req
+    .on('data', chunk => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+
+      let status = 404;
+
+      response = {
+        success: false,
+        data: null
+      };
+
+      if (method === 'GET' && url === '/todos') {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === 'POST' && url === '/todos') {
+        const { id, text } = JSON.parse(body);
+        if (!id || !text) {
+          status = 400;
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+      res.writeHead(status, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(response));
+    });
 });
 
 const PORT = 5000;
