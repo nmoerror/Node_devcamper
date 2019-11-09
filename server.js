65ffab611e101e0e63bd5c48f160f048ffa399cd
Alejandro Gonzalez <Alejandro@nomarginoferror.com>
2019-11-10 09:31:25 +1300

    dotenv

--- server.js
+++ server.js
@@ -1,51 +1,13 @@
-const http = require('http');
+const express = require('express');
+const dotenv = require('dotenv');
 
-const todos = [
-  { id: 1, todo: 'asddasd' },
-  { id: 2, todo: 'asddasd' },
-  { id: 3, todo: 'asddasd' },
-  { id: 4, todo: 'asddasd' },
-  { id: 5, todo: 'asddasd' }
-];
+//load env
+dotenv.config({ path: './config/config.env' });
 
-const server = http.createServer((req, res) => {
-  const { url, method } = req;
-  let body = [];
+const app = express();
 
-  req
-    .on('data', chunk => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
+const PORT = process.env.PORT || 5000;
 
-      let status = 404;
-
-      response = {
-        success: false,
-        data: null
-      };
-
-      if (method === 'GET' && url === '/todos') {
-        status = 200;
-        response.success = true;
-        response.data = todos;
-      } else if (method === 'POST' && url === '/todos') {
-        const { id, text } = JSON.parse(body);
-        if (!id || !text) {
-          status = 400;
-        } else {
-          todos.push({ id, text });
-          status = 201;
-          response.success = true;
-          response.data = todos;
-        }
-      }
-      res.writeHead(status, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(response));
-    });
-});
-
-const PORT = 5000;
-
-server.listen(PORT, () => console.log(`Running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
